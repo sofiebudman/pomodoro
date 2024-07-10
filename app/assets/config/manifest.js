004439018805444811c403631806312b8ab9a1f2
Sofie Budman <changbudmanfamily@gmail.com>
2024-07-09 20:14:51 -0700

    incorporate JS

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,10 @@
+//= require jquery
+//= require rails-ujs
+//= require_tree .
+//= link pomodoro.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+
+
---
+++ app/assets/config/pomodoro.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    var buzzer = $("#buzzer")[0];
+   
+    $("#reset").hide();
+    
+});
+
